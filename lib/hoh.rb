70e0821bf723fde90a00512e94e07e7aa3c334f2
Denise Magner <deerenae@gmail.com>
2020-02-07 05:17:44 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -41,5 +41,11 @@ def naming_system
   #  Species
   # So, if we have the "Kingdom" node we should be able to "tunnel" into the
   # HoH all the way to Species!
-
+  bio_life = {
+    :kingdom =>{
+      :phylum=>{}
+      :class=>{}
+    } 
+    
+  }
 end
